fd8df55958d65e49dd2c6e2a22d3461328d55512
Aki Tuomi <cmouse@desteem.org>
2014-09-16 22:50:00 +0300

    Implement showHelp

--- dhcpd/dhcpd-main.cc
+++ dhcpd/dhcpd-main.cc
@@ -1,11 +1,21 @@
 #include "powerdhcp.hh"
 
+using namespace PowerDHCP;
+
 int main(int argc, const char *argv[]) {
   try {
-    PowerDHCP::initialize(argc, argv);
+    initialize(argc, argv);
   } catch (PowerDHCP::Exception &pde) {
     std::cerr << "Could not initialize: " << pde.reason << std::endl;
   }
 
+  L<<Logger::INFO<<"Hello, world";
+
   return 0; 
 };
+
+void PowerDHCP::showHelp(const std::string& progName) {
+  std::cerr << "Usage: " << progName << " [options]" << std::endl;
+  std::cerr << std::endl; 
+  std::cerr << "    " << "You can use all configuration file options as parameters by prefixing them with --" << std::endl;
+}
